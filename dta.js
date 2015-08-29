058460530f50c096a3337a3c50b2006f47a7a1fa
Arne Schubert <atd.schubert@gmail.com>
2015-08-29 13:53:55 +0200

    #3: Give possibility to pass a function instead of an array

--- dta.js
+++ dta.js
@@ -30,8 +30,12 @@
         }
         return false;
     };
-    dta = function dta(translator, arr) {
+    dta = function dta(translator, arrOrFn) {
         var i, hash, result, tmpName, tmpType;
+
+        if (typeof translator !== 'object') {
+            throw new Error('Can not handle translator object');
+        }
 
         translator.prototypes = translator.prototypes || {};
         translator.prototypes.array = translator.prototypes.array || Array;
@@ -47,12 +51,31 @@
 
         translator.mandatory = translator.mandatory || [];
 
-        for (i = 0; i < arr.length; i += 1) {
-            tmpType = typeof arr[i];
+        if (typeof arrOrFn === 'function') {
+            return function () {
+                return arrOrFn(dta(translator, arguments));
+            };
+        } else if (Array.prototype.isPrototypeOf(arrOrFn)) {
+            for (i = 0; i < arrOrFn.length; i += 1) {
+                tmpType = typeof arrOrFn[i];
 
-            if (tmpType === 'object') {
-                // This could be a lot of prototypes or just an object
-                if (!findPrototype(translator, result, arr[i])) {
+                if (tmpType === 'object') {
+                    // This could be a lot of prototypes or just an object
+                    if (!findPrototype(translator, result, arrOrFn[i])) {
+                        if (!translator[tmpType] || translator[tmpType].length === 0) {
+                            throw new Error('Can not handle argument number ' + i);
+                        }
+                        tmpName = translator[tmpType].splice(0, 1)[0];
+                        if (tmpName.substr(-1) === ']' && tmpName.substr(0, 1) === '[') {
+                            translator[tmpType].unshift(tmpName);
+                            tmpName = tmpName.substring(1, tmpName.length - 1);
+                            result[tmpName] = result[tmpName] || [];
+                            result[tmpName].push(arrOrFn[i]);
+                        } else {
+                            result[tmpName] = arrOrFn[i];
+                        }
+                    }
+                } else {
                     if (!translator[tmpType] || translator[tmpType].length === 0) {
                         throw new Error('Can not handle argument number ' + i);
                     }
@@ -61,34 +84,22 @@
                         translator[tmpType].unshift(tmpName);
                         tmpName = tmpName.substring(1, tmpName.length - 1);
                         result[tmpName] = result[tmpName] || [];
-                        result[tmpName].push(arr[i]);
+                        result[tmpName].push(arrOrFn[i]);
                     } else {
-                        result[tmpName] = arr[i];
+                        result[tmpName] = arrOrFn[i];
                     }
                 }
-            } else {
-                if (!translator[tmpType] || translator[tmpType].length === 0) {
-                    throw new Error('Can not handle argument number ' + i);
-                }
-                tmpName = translator[tmpType].splice(0, 1)[0];
-                if (tmpName.substr(-1) === ']' && tmpName.substr(0, 1) === '[') {
-                    translator[tmpType].unshift(tmpName);
-                    tmpName = tmpName.substring(1, tmpName.length - 1);
-                    result[tmpName] = result[tmpName] || [];
-                    result[tmpName].push(arr[i]);
-                } else {
-                    result[tmpName] = arr[i];
+            }
+
+            for (i = 0; i < translator.mandatory.length; i += 1) {
+                if (!result.hasOwnProperty(translator.mandatory[i])) {
+                    throw new Error('\'' + translator.mandatory[i] + '\' is mandatory, but not set.');
                 }
             }
+            return result;
         }
 
-        for (i = 0; i < translator.mandatory.length; i += 1) {
-            if (!result.hasOwnProperty(translator.mandatory[i])) {
-                throw new Error('\'' + translator.mandatory[i] + '\' is mandatory, but not set.');
-            }
-        }
-
-        return result;
+        throw new Error('Can not handle arguments');
     };
 
     define('dta', function () {
